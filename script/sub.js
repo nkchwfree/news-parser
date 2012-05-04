b25b943053d94cbe7a933ff5841ae695536fbe95
nkchwfree <chwfree@gmail.com>
2012-05-04 17:10:23 +0800

    add

---
+++ script/sub.js
@@ -0,0 +1,41 @@
+var exec = require('child_process').exec;
+var config = require('../config/config').config;
+
+process.on('message', function(data) {
+    //子进程的内存限制默认是64M。
+    var memory_limit = config[memory_limit]||64;
+    if(process.memoryUsage().rss > ( memory_limit * 1024 * 1024)) {
+        process.exit();
+    }
+
+    //获取tidy的处理策略
+    var strategy = config.tidy_strategy[data.data.site+'.'+data.data.type] || 'default';
+
+    //调用tidy允许使用的内存限制，默认是2M。
+    var max_buffer = config.max_buffer||2;
+    exec(config.php+' '+__dirname+'/tidy.php '+ strategy, {maxBuffer:max_buffer*1024*1024}, function(error, body, stderr){
+        if ( !error ) {
+            var jsdom = require('jsdom');
+            jsdom.env(body, [__dirname+'/jquery-1.7.1.min.js'], function(errors, window) {
+                if( errors ){
+                    process.send( { 'key': data.key, 'error': errors } );
+                } else {
+                    window.__stopAllTimers();
+
+                    try {
+                        var parser = require('../lib/matcher/'+data.data.site+'.'+data.data.type);
+                        parser.parse( window, data.data, function( error, match ){
+                            process.send( { 'key': data.key, 'data': match, 'error': error } );
+                        });
+                    }
+                    catch(e) {
+                        console.log(e);
+                    }
+                }
+            });
+        }
+        else {
+            process.send( { 'key': data.key, 'error': 'tidy-error '+error } );
+        }
+    }).stdin.end( data.data.text, 'binary' );
+});
---
+++ script/tidy.php
@@ -0,0 +1,40 @@
+<?php
+
+$strategy = $argv[1];
+$buffer = file_get_contents('php://stdin');
+
+if($strategy=='default') {
+
+    $tidy_options = array(
+        'input-xml'    => false,
+        'output-xml'   => true,
+        'indent'       => true,
+        'wrap'         => false,
+        'hide-comments' => true,
+        'drop-font-tags' => true,
+        'drop-empty-paras' => true,
+        "logical-emphasis" => true,
+        'escape-cdata'  => true
+      );
+    $tidy = new tidy();
+
+    if(preg_match('#charset=([-a-z0-9]+)#msi', $buffer, $match)) {
+        if(strtoupper($match[1])!='UTF-8') {
+            if(strtoupper($match[1])=='GB2312') {
+                $match[1] = 'GB18030';
+            }
+            $buffer = iconv($match[1], 'UTF-8//IGNORE', $buffer);
+        }
+        //print_r($match);
+    }
+
+    //exit;
+
+    $tidy->parseString($buffer, $tidy_options, 'utf8');
+    $tidy->cleanRepair();
+    echo $tidy;
+}
+else if($strategy=='original.gbk') { //原始网页内容是GBK，只需要转换编码
+    echo iconv("GBK", 'UTF-8//IGNORE', $buffer);
+}
+?>
