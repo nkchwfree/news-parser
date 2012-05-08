55f38f81ec5a1c9bac8c41d80c6e841235ca7fec
nkchwfree <chwfree@gmail.com>
2012-05-08 14:32:58 +0800

    add

--- parser.js
+++ parser.js
@@ -1,47 +1,23 @@
-var request = require('request');
 var argv = require('optimist').argv;
-
-var config = require('./config/config').config;
+var develop = require('./lib/develop');
 
 var url = argv.url;
-var data = {};
 
 if(argv.del==1) {
     //删除错误用例样本数据
 
 }
 else {
-    request({ 'url' : url, 'encoding' : 'binary', 'timeout' : 50000 }, function(error, response, body) {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            //console.log(body.length);
-            var parser = require("./lib/parser").parser;
-            parser(body, data, function(error, match){
-                if(!error) {
-                    console.log(match.title);
-                    console.log('---------------------');
-                    console.log(match.content);
-                    //console.log('---------------------');
-                    //console.log(match);
+    develop.parseDetail(url, function(data){
+        console.log(data.title);
+        console.log('---------------------');
+        console.log(data.content);
 
-                    //添加到测试用例中
-                    if(argv.add=="1") {
-                        var redis = require('./lib/redis').redis;
-                        redis.HMSET("test_case", url, JSON.stringify({
-                            url:url,
-                            html:body,
-                            title:match.title,
-                            content:match.content
-                        }));
-                        redis.quit();
-                    }
-                }
-                else {
-                    console.log(error);
-                }
-            });
+        //添加到测试用例中
+        if(argv.add=="1") {
+            var redis = require('./lib/redis').redis;
+            redis.HMSET("test_case", url, JSON.stringify(data));
+            redis.quit();
         }
     });
 }
