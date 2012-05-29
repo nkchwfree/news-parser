1786887d1dee744ec001582f9d7224b3d4832c7e
nkchwfree <chwfree@gmail.com>
2012-05-29 14:35:01 +0800

    fixed bug

--- tests/main.js
+++ tests/main.js
@@ -3,7 +3,6 @@ var fs =  require('fs');
 var vows = require('vows');
 var mysql = require('mysql');
 var config = require('../config/config').config;
-var client = mysql.createClient( config.mysql );
 var _ = require('underscore');
 var redis = require('../lib/redis').redis;
 
@@ -39,38 +38,6 @@ redis.hvals("test_case", function (err, replies) {
     parser.export(module);
     redis.quit();
 });
-/*
-client.query(
-    "select * FROM test_case order by id asc",
-    function(error, results, fields) {
-        if (error) {
-            console.log(error);
-            return;
-        }
-
-        _.each(results,function(row, key){
-            //console.log(row);
-            var obj = {};
-            obj[row.id] = {
-    	        topic:function(){
-    	            parse_one_page(row.html_content, this.callback);
-    	            //console.log(row.url);
-    	        },
-    	        'content':function(error, match){
-    	            assert.equal(match.content, row.content);
-    	        },
-    	        'title':function(error, match){
-    	            assert.equal(match.title, row.title);
-    	        }
-    	    };
-            parser.addBatch(obj);
-        });
-
-        parser.export(module);
-        client.end();
-    }
-);
-*/
 
 
 
