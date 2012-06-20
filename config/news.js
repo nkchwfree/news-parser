16628605001dec5aaf2848d9eb840ea6fe76c504
nkchwfree <chwfree@gmail.com>
2012-06-20 11:38:16 +0800

    添加分析列表页面的程序，通过配置信息来解析

---
+++ config/news.js
@@ -0,0 +1,12 @@
+exports.list = [
+    {
+        name:'新浪滚动-财经'
+        url:'http://roll.finance.sina.com.cn/s/channel.php?ch=03#col=43&spec=&type=&ch=03&k=&offset_page=0&offset_num=0&num=60&asc=&page=1',
+        path:'#d_list .c_tit a'
+    },
+    {
+        name:'腾讯-股票-上市公司'
+        url:'http://finance.qq.com/l/stock/shsgs/index.htm',
+        path:'.newslist a'
+    }
+];
--- parser_list.js
+++ parser_list.js
@@ -3,17 +3,36 @@ var _ = require('underscore');
 var develop = require('./lib/develop');
 
 var url = argv.url;
+var baidu = argv.baidu;
 
-develop.parseList(url, function(list_data){
-    //console.log(list_data);
-    _.each(list_data.list, function(url){
-        //console.log(url)
-        develop.parseDetail(url, function(data){
-            console.log(url)
-            console.log(data.title);
-            console.log('---------------------');
-            console.log(data.content);
-            console.log("======================================================================================================\n\n\n");
+if(baidu) {
+    develop.parseBaiduList(url, function(list_data){
+        //console.log(list_data);
+        _.each(list_data.list, function(url){
+            //console.log(url)
+            develop.parseDetail(url, function(data){
+                console.log(url)
+                console.log(data.title);
+                console.log('---------------------');
+                console.log(data.content);
+                console.log("======================================================================================================\n\n\n");
+            });
         });
     });
-});
+}
+else {
+    develop.parseList(url, {path:".newslist a"}, function(list_data){
+        console.log(list_data.list.length);
+        _.each(list_data.list, function(url){
+            console.log(url);
+
+            develop.parseDetail(url, function(data){
+                console.log(url)
+                console.log(data.title);
+                console.log('---------------------');
+                console.log(data.content);
+                console.log("======================================================================================================\n\n\n");
+            });
+        });
+    });
+}
